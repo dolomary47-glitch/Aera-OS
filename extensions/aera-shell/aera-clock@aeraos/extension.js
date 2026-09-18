d46f71913ffc27d1ce9f890b66d4f010ac9a886f
dolomary47-glitch <dolomary47@gmail.com>
2026-09-18 21:28:32 +0000

    change #3

--- extensions/aera-shell/aera-clock@aeraos/extension.js
+++ extensions/aera-shell/aera-clock@aeraos/extension.js
@@ -1,61 +1,103 @@
 import Clutter from 'gi://Clutter';
 import GLib from 'gi://GLib';
-import GObject from 'gi://GObject';
 import Pango from 'gi://Pango';
 import St from 'gi://St';
 
 import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-
-const AeraClockButton = GObject.registerClass(
-class AeraClockButton extends PanelMenu.Button {
-    _init() {
-        super._init(0.5, 'Aera Clock', true);
-
-        this.add_style_class_name('aera-clock-button');
-
-        this._label = new St.Label({
-            style_class: 'aera-clock-label',
-            text: '',
-            y_align: Clutter.ActorAlign.CENTER,
-        });
-
-        if (this._label.clutter_text)
-            this._label.clutter_text.ellipsize = Pango.EllipsizeMode.NONE;
-
-        this.add_child(this._label);
-    }
-
-    setTime(timeString) {
-        this._label.set_text(timeString);
-    }
-});
 
 export default class AeraClockExtension extends Extension {
     enable() {
-        // Defensive cleanup in case of re-enable or previous unclean state
-        if (Main.panel.statusArea[this.uuid]) {
+        // Clean up any previous panel indicator if upgrading
+        if (Main.panel.statusArea && Main.panel.statusArea[this.uuid]) {
             try {
                 Main.panel.statusArea[this.uuid].destroy();
-            } catch (e) {
-                delete Main.panel.statusArea[this.uuid];
-            }
+            } catch (e) {}
+            delete Main.panel.statusArea[this.uuid];
         }
 
-        this._indicator = new AeraClockButton();
-        Main.panel.addToStatusArea(this.uuid, this._indicator, 0, 'center');
-
+        this._buildWidget();
         this._updateTime();
         this._scheduleNextTick();
+
+        this._monitorsChangedId = Main.layoutManager.connect(
+            'monitors-changed', () => this._reposition()
+        );
+    }
+
+    _buildWidget() {
+        this._destroyWidget();
+
+        this._timeLabel = new St.Label({
+            style_class: 'aera-clock-time',
+            style: 'font-size: 78px; font-weight: 700; color: #cad8e6; text-align: center;',
+            text: '',
+            x_align: Clutter.ActorAlign.CENTER,
+            x_expand: true,
+        });
+
+        this._dateLabel = new St.Label({
+            style_class: 'aera-clock-date',
+            style: 'font-size: 18px; font-weight: 500; color: #9cb0c3; text-align: center; padding-top: 6px;',
+            text: '',
+            x_align: Clutter.ActorAlign.CENTER,
+            x_expand: true,
+        });
+
+        if (this._timeLabel.clutter_text)
+            this._timeLabel.clutter_text.ellipsize = Pango.EllipsizeMode.NONE;
+
+        if (this._dateLabel.clutter_text)
+            this._dateLabel.clutter_text.ellipsize = Pango.EllipsizeMode.NONE;
+
+        this._container = new St.BoxLayout({
+            name: 'AeraClockWidget',
+            style_class: 'aera-clock-widget',
+            vertical: true,
+            reactive: false,
+            can_focus: false,
+            track_hover: false,
+            x_align: Clutter.ActorAlign.CENTER,
+            y_align: Clutter.ActorAlign.CENTER,
+        });
+
+        this._container.add_child(this._timeLabel);
+        this._container.add_child(this._dateLabel);
+
+        // Add behind windows onto the desktop wallpaper layer
+        Main.layoutManager._backgroundGroup.add_child(this._container);
+
+        this._reposition();
+
+        this._repositionTimeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 50, () => {
+            this._reposition();
+            this._repositionTimeoutId = null;
+            return GLib.SOURCE_REMOVE;
+        });
+    }
+
+    _reposition() {
+        if (!this._container)
+            return;
+
+        const monitor = Main.layoutManager.primaryMonitor;
+        if (!monitor)
+            return;
+
+        this._container.set_width(monitor.width);
+
+        // Position in the upper-middle area (approx 22% from top, matching the Aera OS concept)
+        const y = monitor.y + Math.round(monitor.height * 0.22);
+        this._container.set_position(monitor.x, y);
     }
 
     _updateTime() {
-        if (!this._indicator)
+        if (!this._timeLabel || !this._dateLabel)
             return;
 
         const now = GLib.DateTime.new_now_local();
-        this._indicator.setTime(now.format('%H:%M'));
+        this._timeLabel.set_text(now.format('%H:%M'));
+        this._dateLabel.set_text(now.format('%a %e %b %Y').replace(/\s+/g, ' ').trim());
     }
 
     _scheduleNextTick() {
@@ -82,12 +124,33 @@ export default class AeraClockExtension extends Extension {
         }
     }
 
+    _destroyWidget() {
+        if (this._repositionTimeoutId) {
+            GLib.source_remove(this._repositionTimeoutId);
+            this._repositionTimeoutId = null;
+        }
+
+        if (this._container) {
+            const parent = this._container.get_parent();
+            if (parent)
+                parent.remove_child(this._container);
+
+            this._container.destroy();
+            this._container = null;
+        }
+
+        this._timeLabel = null;
+        this._dateLabel = null;
+    }
+
     disable() {
         this._stopTimer();
 
-        if (this._indicator) {
-            this._indicator.destroy();
-            this._indicator = null;
+        if (this._monitorsChangedId) {
+            Main.layoutManager.disconnect(this._monitorsChangedId);
+            this._monitorsChangedId = null;
         }
+
+        this._destroyWidget();
     }
 }
